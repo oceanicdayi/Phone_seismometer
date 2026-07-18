7ecee18c7aa8549c248f33dcba2350231cf40c6c
oceanicdayi <oceanicdayi@gmail.com>
2026-07-18 08:50:10 +0800

    Update offline cache for split scripts

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'mobile-seismograph-v2';
-const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon.svg'];
+const ASSETS = ['./','./index.html','./styles.css','./app-core.js','./app-storage-ui.js','./app-github-init.js','./manifest.webmanifest','./assets/icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
